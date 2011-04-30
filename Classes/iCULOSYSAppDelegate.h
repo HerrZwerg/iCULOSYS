ec2be9ccda1306acce1ce980ef4e95ef08023124
Alfred Dworschak <alfred.dworschak@brain-behind.com>
2011-04-30 21:25:45 +0200

    iCULOSYS

---
+++ Classes/iCULOSYSAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  iCULOSYSAppDelegate.h
+//  iCULOSYS
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface iCULOSYSAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
